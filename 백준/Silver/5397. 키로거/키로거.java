a6ba839bc893585fa3dc6ad3cd8fef60109627cd
jini-zzang <92137309+jini-zzang@users.noreply.github.com>
2023-03-29 18:42:52 +0900

    [Silver II] Title: 키로거, Time: 1156 ms, Memory: 70096 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Stack<Character> lstack = new Stack<>();
+        Stack<Character> rstack = new Stack<>();
+
+        int len = Integer.parseInt(br.readLine());
+        for(int i=0; i<len; i++){
+            char[] ch = br.readLine().toCharArray();
+            for(int j=0; j<ch.length; j++){
+                if(ch[j]=='<'){
+                    if(!lstack.isEmpty())
+                        rstack.push(lstack.pop());
+                }else if(ch[j]=='>'){
+                    if(!rstack.isEmpty())
+                        lstack.push(rstack.pop());
+                }else if(ch[j]=='-'){
+                    if(!lstack.isEmpty())
+                        lstack.pop();
+                }else{
+                    lstack.push(ch[j]);
+                }
+            }
+            while(!lstack.isEmpty()){
+                rstack.push(lstack.pop());
+            }
+            while(!rstack.isEmpty()){
+                bw.append(rstack.pop());
+            }
+            bw.append('\n');
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
