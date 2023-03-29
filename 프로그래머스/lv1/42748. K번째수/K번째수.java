f143c9d8a5aae8843b1b66fd5b81d0929dd8db96
jini-zzang <92137309+jini-zzang@users.noreply.github.com>
2023-03-29 19:27:56 +0900

    [level 1] Title: K번째수, Time: 0.32 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i=0; i<answer.length; i++){
+            int[] copy = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(copy);
+            answer[i] = copy[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
