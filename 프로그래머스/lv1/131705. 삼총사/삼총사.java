2ad6bfd1304cd981fee30468a4b9023453053911
jini-zzang <92137309+jini-zzang@users.noreply.github.com>
2023-03-29 19:20:41 +0900

    [level 1] Title: 삼총사, Time: 0.35 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0, len = number.length;
+        Arrays.sort(number);
+        for(int i=0; number[i]<=0 && i<len-2; i++){
+            for(int j=i+1; j<number.length-1; j++){
+                for(int k=j+1; k<number.length; k++){
+                    if(number[i]+number[j]+number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
