659c2735c653ce306b30dd6e8e00cd3b0c5f55db
jini-zzang <92137309+jini-zzang@users.noreply.github.com>
2023-03-29 18:47:04 +0900

    [Silver I] Title: 경로 찾기, Time: 244 ms, Memory: 22224 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,42 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        String[][] G = new String[n+1][n+1];
+        for(int i=0; i<n; i++){
+            G[i] = br.readLine().split(" ");
+        }
+
+        int[][] result = new int[n][n];
+        for(int i=0;i<n; i++){
+            Queue<Integer> pq = new LinkedList<>();
+            boolean[] visit = new boolean[n];
+            pq.add(i);
+            while(!pq.isEmpty()){
+                int j = pq.poll();
+                if(visit[j]) continue;
+                visit[j] = true;
+                for(int k=0; k<n; k++){
+                    if(G[j][k].equals("1")){
+                        result[i][k] = 1;
+                        pq.add(k);
+                    }
+                }
+            }
+        }
+        for(int[] i : result){
+            for(int j : i)
+                sb.append(j).append(" ");
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
