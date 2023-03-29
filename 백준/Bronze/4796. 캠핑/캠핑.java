703819e3b74f756512af9a47b6a6b156c2a1374e
jini-zzang <92137309+jini-zzang@users.noreply.github.com>
2023-03-29 18:42:36 +0900

    [Bronze I] Title: 캠핑, Time: 144 ms, Memory: 14484 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int l,p,v,days, i=1;
+        while(true){
+            String[] testCase = br.readLine().split(" ");
+            l = Integer.parseInt(testCase[0]);
+            p = Integer.parseInt(testCase[1]);
+            v = Integer.parseInt(testCase[2]);
+            if(l==0 && p==0 && v==0) break;
+            days = ((v/p)*l) + Math.min(l, v%p);
+            sb.append("Case ").append(i++).append(": ").append(days).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
