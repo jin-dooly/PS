c5c9a206b8f8707d9040220b6597279a2264caed
jini-zzang <92137309+jini-zzang@users.noreply.github.com>
2023-03-29 18:45:21 +0900

    [Silver V] Title: 나이순 정렬, Time: 2372 ms, Memory: 314908 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int len = Integer.parseInt(br.readLine());
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<len; i++){
+            list.add(br.readLine());
+        }
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String[] str1 = o1.split(" ");
+                String[] str2 = o2.split(" ");
+
+                return Integer.parseInt(str1[0]) - Integer.parseInt(str2[0]);
+            }
+        });
+        for(int i=0; i<len; i++){
+            System.out.println(list.get(i));
+        }
+    }
+}
