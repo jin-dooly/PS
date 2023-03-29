016db4c84c686cfb09adf81888fb400b39c6ee3e
jini-zzang <92137309+jini-zzang@users.noreply.github.com>
2023-03-29 18:45:33 +0900

    [Silver IV] Title: 스택, Time: 452 ms, Memory: 34976 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int[] stack = new int[10000];
+        int top=-1;
+
+        int len = sc.nextInt();
+        String order = "";
+
+        for(int i=0; i<len; i++){
+            order = sc.next();
+            if(order.equals("push")){
+                stack[++top] = sc.nextInt();
+            }
+            else if(order.equals("pop")){
+                if(top!=-1)
+                    sb.append(stack[top--]+"\n");
+                else
+                    sb.append("-1\n");
+            }
+            else if (order.equals("size")){
+                sb.append((top+1)+"\n");
+            }
+            else if (order.equals("empty")) {
+                if(top==-1)
+                    sb.append(1+"\n");
+                else
+                    sb.append(0+"\n");
+            }
+            else if (order.equals("top")) {
+                if(top!=-1)
+                    sb.append(stack[top]+"\n");
+                else
+                    sb.append(-1+"\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
