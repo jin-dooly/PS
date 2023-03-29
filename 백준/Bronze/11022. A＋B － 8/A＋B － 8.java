61d1658ae45de9501132ae481beffbf3b5d6d939
jini-zzang <92137309+jini-zzang@users.noreply.github.com>
2023-03-29 18:46:47 +0900

    [Bronze V] Title: A+B - 8, Time: 268 ms, Memory: 18964 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        Scanner sc=new Scanner(System.in);
+        BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+        int t=sc.nextInt();
+        int a,b;
+        for(int i=1;i<=t;i++){
+            a=sc.nextInt();
+            b=sc.nextInt();
+            bw.write("Case #"+i+": "+a+" + "+b+" = "+(a+b)+"\n");
+        }
+        bw.close();
+    }
+}
