f6b2aff0b483cb8a4fafaec94ec2989b4a893c5a
jini-zzang <92137309+jini-zzang@users.noreply.github.com>
2023-03-29 18:39:06 +0900

    [Bronze III] Title: 홀수, Time: 204 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int sum = 0;
+        int min = 100;
+
+        for(int i=0; i<7; i++){
+            if((n=sc.nextInt())%2 == 1){
+                sum += n;
+                if(min>n)
+                    min = n;
+            }
+        }
+        if(sum==0)
+            System.out.println("-1");
+        else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+}
