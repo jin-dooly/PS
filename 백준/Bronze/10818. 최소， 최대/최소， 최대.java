42901110de9994bb80af545a5e02b5bc5d89db38
jini-zzang <92137309+jini-zzang@users.noreply.github.com>
2023-03-29 18:45:27 +0900

    [Bronze III] Title: 최소, 최대, Time: 1692 ms, Memory: 317840 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr= new int[n];
+        int min=0,max=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        min=arr[0];
+        max=arr[0];
+        for(int i=0;i<n;i++){
+            if(min>arr[i]){min=arr[i];}
+            else if(max<arr[i]){max=arr[i];}
+        }
+        System.out.print(min+" "+max);
+    }
+}
