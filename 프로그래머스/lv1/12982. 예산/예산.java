aae2b1fdcbdedc7f1991d34118d783d9a6f4b342
jini-zzang <92137309+jini-zzang@users.noreply.github.com>
2023-03-29 19:28:46 +0900

    [level 1] Title: 예산, Time: 0.32 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0, sum = 0;
+        Arrays.sort(d);
+        for(int i : d){
+            sum += i;
+            if(sum > budget) break;
+            answer++;
+        }
+        return answer;
+    }
+}
