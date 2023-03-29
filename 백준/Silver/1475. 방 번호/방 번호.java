ae5e5084b68d572fd2e04d629c73878d3351d6c3
jini-zzang <92137309+jini-zzang@users.noreply.github.com>
2023-03-29 18:34:08 +0900

    [Silver V] Title: 방 번호, Time: 208 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] cnt = new int[9];
+        Arrays.fill(cnt, 0);
+        int max = 0;
+        char[] numbers = sc.next().toCharArray();
+        for(int i=0; i<numbers.length; i++){
+            int ind = numbers[i]-'0';
+
+            if(ind==9)
+                ind = 6;
+            cnt[ind]++;
+
+            if(ind == 6){
+                if(max< (cnt[6]+1)/2)
+                    max = ((cnt[6] + 1) / 2);
+            } else if(max < cnt[ind]){
+                max = cnt[ind];
+            }
+        }
+        System.out.println(max);
+    }
+}
