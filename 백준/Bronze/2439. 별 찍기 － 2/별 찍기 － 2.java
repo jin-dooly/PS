cb7b99bf5fa2bd637dcffa574aca58e059f51cd6
jini-zzang <92137309+jini-zzang@users.noreply.github.com>
2023-03-29 18:38:05 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 216 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        Scanner sc=new Scanner(System.in);
+        BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+        int t=sc.nextInt();
+        for(int i=1;i<=t;i++){
+            if(i<t){
+                for(int k=0;k<(t-i);k++){
+                    bw.write(" ");
+                }
+            }
+            for(int j=0;j<i;j++){
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+        bw.close();
+    }
+}
