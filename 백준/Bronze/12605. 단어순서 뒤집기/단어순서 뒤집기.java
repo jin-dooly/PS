c2bc95c36030170eb163da86daa173b581f7b507
jini-zzang <92137309+jini-zzang@users.noreply.github.com>
2023-03-29 18:48:00 +0900

    [Bronze I] Title: 단어순서 뒤집기, Time: 220 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        int len = sc.nextInt();
+        String[] strings = new String[len];
+        sc.nextLine();
+        for(int i=0; i<len; i++){
+            strings[i] = sc.nextLine();
+        }
+        for(int i=0; i<len; i++){
+            String[] words = strings[i].split(" ");
+            System.out.print("Case #"+(i+1)+": ");
+            for(int j=words.length-1; j>=0; j--){
+                System.out.print(words[j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
