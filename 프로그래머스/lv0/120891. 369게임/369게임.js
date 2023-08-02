c333656e76ed856426cdb00d53b3de853ea19c98
LeeJinIe <92137309+jin-dooly@users.noreply.github.com>
2023-08-03 08:50:05 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -0,0 +1,8 @@
+function solution(order) {
+    var answer = 0;
+    order = order.toString();
+    for(var i=0; i<order.length; i++){
+        if(order[i]=='3' || order[i]=='6' || order[i]=='9') answer++;
+    }
+    return answer;
+}
