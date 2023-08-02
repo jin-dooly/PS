f0d808b9c5f9d38d1ee5dd874e42973bbc8a394f
LeeJinIe <92137309+jin-dooly@users.noreply.github.com>
2023-08-03 07:44:22 +0900

    [level 0] Title: 약수 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = new Set();
+    for(var i=1; i<=Math.sqrt(n); i++){
+        if(n%i==0){
+            answer.add(i);
+            answer.add(n/i);
+        }
+    }
+    return [...answer].sort((a, b) => a-b);
+}
