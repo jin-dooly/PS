e7262c25c378f5e16d72a5c226f497c9ada08ebc
LeeJinIe <92137309+jin-dooly@users.noreply.github.com>
2023-08-07 19:26:46 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.35 ms, Memory: 68.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        int len = numbers.length;
+        int answer = numbers[len-1]*numbers[len-2] > numbers[0]*numbers[1] ? numbers[len-1]*numbers[len-2] : numbers[0]*numbers[1];
+        return answer;
+    }
+}
