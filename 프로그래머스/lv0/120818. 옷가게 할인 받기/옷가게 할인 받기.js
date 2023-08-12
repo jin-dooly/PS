77c707d2044cd8220c08fb60235e43c7fc272cee
LeeJinIe <92137309+jin-dooly@users.noreply.github.com>
2023-08-12 21:07:04 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,10 @@
+function solution(price) {
+    if(price >= 500000){
+        price -= price*0.2;
+    }else if(price >= 300000){
+        price -= price*0.1;
+    }else if(price >= 100000){
+        price -= price*0.05;
+    }
+    return Math.floor(price);
+}
