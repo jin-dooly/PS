fb72887c5146a1427cdd2644f270043e0a385b24
JinIe Lee <92137309+jin-dooly@users.noreply.github.com>
2023-10-02 23:54:00 +0900

    [level 1] Title: 모의고사, Time: 2.47 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/README.md
+++ 프로그래머스/lv1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 34 MB, 시간: 1.68 ms
+메모리: 34 MB, 시간: 2.47 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -3,15 +3,9 @@ function solution(answers) {
     var count = [];
     count[0] = answers.filter((n, idx) => idx%5+1 === n).length;
     var value = [2,1,2,3,2,4,2,5];
-    count[1] = answers.filter((n, idx) => {
-        if(value[idx%8] === n) return true;
-        return false;
-    }).length;
+    count[1] = answers.filter((n, idx) => value[idx%8] === n).length;
     value = [3,3,1,1,2,2,4,4,5,5];
-    count[2] = answers.filter((n, idx) => {
-        if(value[idx%10] === n) return true;
-        return false;
-    }).length;
+    count[2] = answers.filter((n, idx) => value[idx%10] === n).length;
     var max = Math.max(...count);
     return count.reduce((arr, cur, idx) => {
         if(cur === max) arr.push(idx+1);
