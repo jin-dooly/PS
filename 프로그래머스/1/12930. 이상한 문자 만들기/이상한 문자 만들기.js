59a1692e195b3e9ef7c9e51830047554110856c2
JinIe Lee <jj074567@gmail.com>
2023-12-10 20:59:02 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,8 +1,6 @@
 function solution(s) {
     var answer = '';
-    return s.split(' ').map(str => [...str].map((s, idx) => {
-            if(idx%2) return s.toLowerCase()
-            return s.toUpperCase()
-        }).join('')
-    ).join(' ')
+    return s.split(' ')
+        .map(str => [...str].map((s, idx) => idx%2 ? s.toLowerCase(): s.toUpperCase()).join(''))
+        .join(' ')
 }
