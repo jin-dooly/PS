f60ffcd95963204feebf1ed04aa02dbc53fe746a
JinIe Lee <jj074567@gmail.com>
2023-12-12 15:10:17 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,12 +1,11 @@
 function solution(bandage, health, attacks) {
-    let answer = 0;
+    let [t, x, y] = bandage;
     let hp = health;
     let time = 0;
     for([atc_time, atc_hp] of attacks){
         var duration = atc_time - time - 1;
-        hp += (Math.floor(duration/bandage[0]) * (bandage[0]*bandage[1]+bandage[2])) + (duration%bandage[0])*bandage[1]
-        hp = Math.min(hp, health);
-        hp -= atc_hp;
+        hp += (Math.floor(duration/t) * (t*x+y)) + (duration%t)*x
+        hp = Math.min(hp, health) - atc_hp;
         time = atc_time;
         if(hp <= 0) return -1
     }
